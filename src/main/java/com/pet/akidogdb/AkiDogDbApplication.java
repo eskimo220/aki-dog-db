6256a84e044b9896fec331295972fbe15504d9f6
wanghaoqi <eskimo220@sina.com>
2021-12-31 13:14:18 +0800

    create project

---
+++ src/main/java/com/pet/akidogdb/AkiDogDbApplication.java
@@ -0,0 +1,13 @@
+package com.pet.akidogdb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AkiDogDbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AkiDogDbApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/pet/akidogdb/AkiDogDbApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pet.akidogdb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AkiDogDbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
